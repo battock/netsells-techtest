23b22892d7c9f0be516eafa241aa71ed2badd64b
Stuart Woodland <smwoodland@hotmail.com>
2020-12-04 16:49:34 +0000

    created hot submission interface for retrofit

--- app/src/main/java/com/firebase/netsells_techtest/data/HotSubmissionsApi.kt
+++ app/src/main/java/com/firebase/netsells_techtest/data/HotSubmissionsApi.kt
@@ -1,5 +1,10 @@
 package com.firebase.netsells_techtest.data
 
+import com.firebase.netsells_techtest.model.HotSubmission
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface HotSubmissionsApi {
-    
+    @GET("")
+    fun getHotSubmissionsList(): Single<List<HotSubmission>>
 }
